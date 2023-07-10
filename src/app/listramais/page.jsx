3366a8e0351ff787edc57b33c1db4a2e1f7d8884
FelipeJustusMatos <felipevalitaa@gmail.com>
2023-07-10 17:05:25 -0300

    ProporcionalDone

--- src/app/listramais/page.jsx
+++ src/app/listramais/page.jsx
@@ -1,11 +1,4 @@
+"use client";
 export default function ListRamaisPage() {
-  return (
-    <>
-      <main className="flex  w-full h-[100vh] ">
-        <h1 className="w-full">
-          <p className="text-center">Estou na página de Lista de Ramais</p>
-        </h1>
-      </main>
-    </>
-  );
+  return <></>;
 }
