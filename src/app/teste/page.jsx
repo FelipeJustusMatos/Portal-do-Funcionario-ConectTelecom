01d2ac84e99c069e3bd030e37b41e6a0a4d64d44
FelipeJustusMatos <felipevalitaa@gmail.com>
2023-08-17 15:42:14 -0300

    teste proporcional simples done

--- src/app/teste/page.jsx
+++ src/app/teste/page.jsx
@@ -1,5 +1,8 @@
 "use client";
-import React, { useState } from "react";
+import { useState } from "react";
+import image5 from "../../../public/img/proppc.webp";
+import image4 from "../../../public/img/propmobile.webp";
+import Image from "next/image";
 
 const DateSubtraction = () => {
   const [startDate, setStartDate] = useState("");
@@ -45,60 +48,109 @@ const DateSubtraction = () => {
   };
 
   return (
-    <div className="">
-      <ul className="w-full">
-        <li>
-          <label>Data Inicial:</label>
-          <input
-            type="date"
-            value={startDate}
-            onChange={handleStartDateChange}
+    <>
+      {" "}
+      <div className="min-w-screen">
+        {" "}
+        <figure className="sm:hidden">
+          <Image
+            src={image5}
+            alt="Cadastrar Ramal"
+            className="w-screen rounded-tl-3xl md:rounded-none h-[30vh] object-cover"
           />
-        </li>
-        <li>
-          <label>Data Final:</label>
-          <input type="date" value={endDate} onChange={handleEndDateChange} />
-        </li>
-        <li className="border rounded-r-lg">
-          <label className="mr-2">Resultado</label>
-          <input
-            type="number"
-            className="text-center"
-            value={calculateDaysPassed()}
-            readOnly
+        </figure>
+        <figure className="-mt-1 ms:hidden">
+          <Image
+            src={image4}
+            alt="Cadastrar Ramal"
+            className="w-screen rounded-none -mt-2 h-[20vh]"
           />
-        </li>
-        <li className="border rounded-r-lg">
-          <label>Valor do Plano</label>
-          <input
-            id="plano"
-            type="number"
-            className="text-center"
-            onChange={handlePlanoChange}
-          />
-        </li>
-        <li>
-          <button onClick={toggleIncludeDay}>
-            {includeDay ? "Não Incluir Dia" : "Incluir Dia"}
-          </button>
-        </li>
-        <li className="border rounded-r-lg">
-          <label>Valor Calculado</label>
-          <input
-            type="text"
-            className="text-center"
-            value={formatCurrency(calculateResult())}
-            readOnly
-          />
-        </li>
-        <li>
-          <p>
-            Fórmula: ({plano} / 30) * {calculateDaysPassed()} ={" "}
-            {formatCurrency(calculateResult())}
-          </p>
-        </li>
-      </ul>
-    </div>
+        </figure>
+        <div className="text-center justify-center items-center self-center  ">
+          <ul className="flex mt-10 sm:mt-5 justify-center sm:flex-col sm:col-span-1 ">
+            <li className=" grid mr-3 sm:mr-0 sm:justify-center">
+              <label className=" w-full">Data Inicial:</label>
+              <input
+                type="date"
+                value={startDate}
+                onChange={handleStartDateChange}
+                className="border border-black rounded-md sm:w-[33vh]"
+              />
+            </li>
+            <li className=" grid ml-3 sm:ml-0 sm:justify-center ">
+              <label>Data Final:</label>
+              <input
+                type="date"
+                value={endDate}
+                onChange={handleEndDateChange}
+                className="border border-black rounded-md sm:w-[33vh]"
+              />
+            </li>
+          </ul>
+        </div>{" "}
+        <div className="text-center mt-5 flex justify-center m-3 bg-pt-3 ">
+          <ul className="flex gap-5 mr-4 grid-cols-2 ">
+            <li>
+              <button
+                className="btn btn-outline-success text-white bg-green-800
+                hover:border-green-800 hover:text-black hover:bg-blue-300"
+                onClick={toggleIncludeDay}
+              >
+                {includeDay ? "Não Incluir Dia" : "Incluir Dia"}
+              </button>
+            </li>
+          </ul>
+        </div>
+        <div className="text-center mt-5 pt-3">
+          <ul className="flex justify-center items-center gap-5 grid-cols-2 sm:flex-col sm:col-span-1 ">
+            <li className="inline-block">
+              <div className="input-group mb-3 sm:mb-0 justify-center ">
+                <span className="input-group-text ">
+                  <p className="w-full ">VALOR - PLANO:</p>
+                </span>
+                <input
+                  id="plano"
+                  type="number"
+                  onChange={handlePlanoChange}
+                  className="form-control border border-black"
+                  placeholder="Ex: 60"
+                />
+              </div>
+            </li>
+            <li className="inline-block">
+              <div className="input-group mb-3 sm:mb-0">
+                <span className="input-group-text">Dias Passados:</span>
+                <input
+                  type="number"
+                  className="text-center"
+                  value={calculateDaysPassed()}
+                  readOnly
+                />
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li className="border rounded-r-lg text-center">
+              <h1 className="w-full ">Resultado</h1>
+              <input
+                type="text"
+                className="text-center"
+                value={formatCurrency(calculateResult())}
+                readOnly
+              />
+            </li>
+            <li className="text-center">
+              <p>
+                Fórmula: ({plano} / 30) * {calculateDaysPassed()} ={" "}
+                {formatCurrency(calculateResult())}
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 };
 
